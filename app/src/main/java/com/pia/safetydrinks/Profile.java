a01eb273126a30bfef9fe9bcca8cb7c6c6eebf5e
JUAN CARLOS MUNOZ BOJORQUEZ <juan.munozbj@uanl.edu.mx>
2020-02-21 14:57:43 -0600

    DataBase and Sign Out

--- app/src/main/java/com/pia/safetydrinks/Profile.java
+++ app/src/main/java/com/pia/safetydrinks/Profile.java
@@ -2,13 +2,36 @@ package com.pia.safetydrinks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Profile extends AppCompatActivity {
+
+    private Button mbtnSignOut;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        mAuth = FirebaseAuth.getInstance();
+        mbtnSignOut = (Button) findViewById(R.id.btnSignOut);
+
+        mbtnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                startActivity(new Intent(Profile.this,MainActivity.class));
+                finish();
+            }
+        });
+
+
     }
 }
